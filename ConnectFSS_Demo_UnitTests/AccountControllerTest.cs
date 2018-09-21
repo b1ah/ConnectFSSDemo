c84dc185e5cbe9039f18c7685a36245f05e1cc49
Kurt Moeller <kurtmoeller.2k4@gmail.com>
2018-09-21 05:11:26 -0600

    Initial unit test commit.

---
+++ ConnectFSS_Demo_UnitTests/AccountControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConnectFSS_Demo.Controllers;
+
+namespace ConnectFSS_Demo_UnitTests
+{
+    [TestClass]
+    public class AccountControllerTest
+    {
+        [TestMethod]
+        public void TestIndexGet()
+        {
+            var ac = new AccountsController();
+            var result = ac.Index();
+            Assert.IsNotNull(result);
+        }
+    }
+}
